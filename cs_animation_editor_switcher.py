2086860e81766bb4d3679082352e65e0359d1863
CenekStrichel <cenda@cenda.cz>
2018-07-16 19:07:58 +0200

    Preferences for addon

--- cs_animation_editor_switcher.py
+++ cs_animation_editor_switcher.py
@@ -34,26 +34,55 @@ bl_info = {
 import bpy
 from bpy.types import Header, Panel
 from bpy.props import StringProperty, IntProperty, BoolProperty, EnumProperty, FloatProperty
-	
+from bpy.types import AddonPreferences, Operator
+        	
 
-	
 ###################################	
 # Animation Header #
 ###################################
 def AnimEditor_Switcher(self, context):
 	
+
+	user_preferences = context.user_preferences
+	addon_prefs = user_preferences.addons[__name__].preferences
+
+
 	layout = self.layout
-	row = layout.row(align=True)
 	
-#	row.operator("scene.animation_editor_switcher", text = "", icon = "TIME").editorType = "TIMELINE"
-	row.operator("scene.animation_editor_switcher", text = "", icon = "IPO").editorType = "GRAPH_EDITOR"
 	
-	row = layout.row(align=True)
-	row.operator("scene.animation_editor_switcher", text = "", icon = "ACTION").editorType = "DOPESHEET_EDITOR"
-	row.operator("scene.animation_editor_switcher", text = "", icon = "OBJECT_DATAMODE").editorType = "DOPESHEET_EDITOR_ACTION"
-	
-	row = layout.row(align=True)
-	row.operator("scene.animation_editor_switcher", text = "", icon = "NLA").editorType = "NLA_EDITOR"
+	# TIMELINE #
+	if( addon_prefs.boolTimeline ):
+		
+		row = layout.row(align=True)
+		row.operator("scene.animation_editor_switcher", text = "", icon = "TIME").editorType = "TIMELINE"
+		
+	# GRAPH #
+	if( addon_prefs.boolGraph or addon_prefs.boolDrivers ):
+		
+		row = layout.row(align=True)
+		
+		if( addon_prefs.boolGraph ):
+			row.operator("scene.animation_editor_switcher", text = "", icon = "IPO").editorType = "GRAPH_EDITOR"
+			
+		if( addon_prefs.boolDrivers ):
+			row.operator("scene.animation_editor_switcher", text = "", icon = "DRIVER").editorType = "GRAPH_EDITOR_DRIVERS"
+		
+	# DOPE #	
+	if( addon_prefs.boolDopesheet or addon_prefs.boolAction ):
+		
+		row = layout.row(align=True)
+		
+		if( addon_prefs.boolDopesheet ):	
+			row.operator("scene.animation_editor_switcher", text = "", icon = "OOPS").editorType = "DOPESHEET_EDITOR"
+			
+		if( addon_prefs.boolAction ):	
+			row.operator("scene.animation_editor_switcher", text = "", icon = "OBJECT_DATAMODE").editorType = "DOPESHEET_EDITOR_ACTION"
+		
+	# NLA #	
+	if( addon_prefs.boolNLA ):
+		
+		row = layout.row(align=True)
+		row.operator("scene.animation_editor_switcher", text = "", icon = "NLA").editorType = "NLA_EDITOR"
 	
 	
 class AnimationEditorSwitcher(bpy.types.Operator):
@@ -74,28 +103,62 @@ class AnimationEditorSwitcher(bpy.types.Operator):
 			bpy.context.area.type = "DOPESHEET_EDITOR"
 			bpy.context.space_data.mode = 'ACTION'
 			
+		elif(self.editorType == "GRAPH_EDITOR"):
+			bpy.context.area.type = "GRAPH_EDITOR"
+			bpy.context.space_data.mode = 'FCURVES'
+			
+		elif(self.editorType == "GRAPH_EDITOR_DRIVERS"):
+			bpy.context.area.type = "GRAPH_EDITOR"
+			bpy.context.space_data.mode = 'DRIVERS'	
+			
 		else:
 			bpy.context.area.type = self.editorType
 			
 		return {'FINISHED'}
 	
 	
+class AnimEditSwitcherAddonPreferences(AddonPreferences):
+
+	bl_idname = __name__
+
+	boolTimeline = BoolProperty( name="Timeline",default=False )
+	boolGraph = BoolProperty( name="Graph Editor",default=True )
+	boolDrivers = BoolProperty( name="Drivers Editor",default=False )
+	boolDopesheet= BoolProperty( name="Dopesheet",default=True )
+	boolAction= BoolProperty( name="Action Editor",default=True )
+	boolNLA= BoolProperty( name="NLA",default=True )
+	
+	def draw(self, context):
+	
+		layout = self.layout
+		layout.prop(self, "boolTimeline")
+		layout.prop(self, "boolGraph")
+		layout.prop(self, "boolDrivers")
+		layout.prop(self, "boolDopesheet")
+		layout.prop(self, "boolAction")
+		layout.prop(self, "boolNLA")
+
+	
 ################################################################
 # register #
 
 def register():
 	bpy.utils.register_module(__name__)
-#	bpy.types.TIME_HT_header.prepend(AnimEditor_Switcher)
+	
+	bpy.types.TIME_HT_header.prepend(AnimEditor_Switcher)
 	bpy.types.GRAPH_HT_header.prepend(AnimEditor_Switcher)
 	bpy.types.DOPESHEET_HT_header.prepend(AnimEditor_Switcher)
 	bpy.types.NLA_HT_header.prepend(AnimEditor_Switcher)
 	
+	
 def unregister():
 	bpy.utils.unregister_module(__name__)
-#	bpy.types.TIME_HT_header(AnimEditor_Switcher)
+	
+	bpy.types.TIME_HT_header(AnimEditor_Switcher)
 	bpy.types.GRAPH_HT_header(AnimEditor_Switcher)
 	bpy.types.DOPESHEET_HT_header(AnimEditor_Switcher)
 	bpy.types.NLA_HT_header(AnimEditor_Switcher)
+	
 	
 if __name__ == "__main__":
 	register()
